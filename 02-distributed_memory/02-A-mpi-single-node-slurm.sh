2199fa4add09ce4a72205c364d2e83b53f698f15
William Oquendo <wfoquendop@unal.edu.co>
2024-12-28 18:37:04 -0500

    Updated scripts for mpi single node

--- 02-distributed_memory/02-A-mpi-single-node-slurm.sh
+++ 02-distributed_memory/02-A-mpi-single-node-slurm.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH --job-name=mpi_sum_single    # Name of the job
-#SBATCH --partition=normal           # Partition (queue) name
+#SBATCH --clusters=fisica            # Partition (queue) name
+#SBATCH --partition=cpu.cecc         # Partition (queue) name
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --ntasks=8                   # Number of MPI processes
-#SBATCH --cpus-per-task=1           # CPU cores per process
-#SBATCH --mem=4G                     # Memory per node
-#SBATCH --time=00:10:00             # Time limit
-#SBATCH --output=%j_mpi_single.out   # Standard output file
-#SBATCH --error=%j_mpi_single.err    # Standard error file
+#SBATCH --cpus-per-task=1            # CPU cores per process
+#SBATCH --mem=14G                    # Memory per node
+#SBATCH --time=00:10:00              # Time limit
+#SBATCH --output=%j_%x.out           # Standard output file
+#SBATCH --error=%j_%x.err            # Standard error file
 
 # Load required modules (modify according to your system)
-module load openmpi
-module load gcc
+# module load openmpi
+# module load gcc
 
 # Compile the program
-mpicxx -O3 vector_sum_mpi.cpp -o vector_sum_mpi
+mpicxx -O3 vector_sum_mpi.cpp -o vector_sum_mpi.x
 
 # Print job information
 echo "Job started at: $(date)"
@@ -22,7 +23,7 @@ echo "Running on node: $(hostname)"
 echo "Number of MPI processes: $SLURM_NTASKS"
 
 # Run the MPI program
-srun ./vector_sum_mpi
+./vector_sum_mpi.x
 
 # Print job end time
 echo "Job finished at: $(date)"
